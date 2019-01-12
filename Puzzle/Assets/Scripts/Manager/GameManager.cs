d07fa064ab9d58e53f737bdce15463abaee20e79
safonce <saf.once28@gmail.com>
2019-01-12 20:26:49 +0800

    Add Game Manager script

---
+++ Puzzle/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            Instance = this;
+        }
+    }
+    
+    public void OpenDoor (GameObject door)
+    {
+        door.SetActive(false);
+    }
+
+    public void CloseDoor(GameObject door)
+    {
+        door.SetActive(true);
+    }
+}
