c382125e5d2c5d80bac51a0786e9a613a5e48871
safonce <saf.once28@gmail.com>
2019-01-12 20:27:21 +0800

    Add pressure plate script

---
+++ Puzzle/Assets/Scripts/Level/PressurePlate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] GameObject triggerableObject = null;
+    [SerializeField] string pressParam = "press";
+    [SerializeField] bool needWeight = true;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger(pressParam);
+
+        if (triggerableObject != null)
+            GameManager.Instance.OpenDoor(triggerableObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (needWeight)
+        {
+            animator.SetTrigger(pressParam);
+
+            if (triggerableObject != null)
+                GameManager.Instance.CloseDoor(triggerableObject);
+        }
+    }
+}
