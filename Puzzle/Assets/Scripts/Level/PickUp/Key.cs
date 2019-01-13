24c11adcd603c01c6c95f7333e36f063a9b45a38
safonce <saf.once28@gmail.com>
2019-01-13 19:07:41 +0800

    Modify behaviour

---
+++ Puzzle/Assets/Scripts/Level/PickUp/Key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+            if (inventory == null)
+                return;
+
+            // Check if inventory has space
+            if (inventory.IsEmpty())
+            {
+                // Add to inventory
+                inventory.AddKey(this);
+                // Destroy key
+                Destroy(gameObject);
+            }
+        }
+    }
+}
