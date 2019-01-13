8e3ecfd892c29e051ea4721964c2a40a8f38929c
safonce <saf.once28@gmail.com>
2019-01-13 19:07:11 +0800

    Add compare tag

---
+++ Puzzle/Assets/Scripts/Level/Props/Box.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerCarry player = other.GetComponent<PlayerCarry>();
+            if (player == null)
+                return;
+
+            if (!player.Carrying)
+                player.itemAvailable = this.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerCarry player = other.GetComponent<PlayerCarry>();
+
+            if (player == null)
+                return;
+
+            if (!player.Carrying)
+                player.itemAvailable = null;
+        }
+    }
+}
