b044f3044fe56059b03f1f78e949996ce79f5762
safonce <saf.once28@gmail.com>
2019-01-13 15:20:17 +0800

    Add box & player carry script

---
+++ Puzzle/Assets/Scripts/Level/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerCarry pc = other.GetComponent<PlayerCarry>();
+
+        if (pc == null)
+            return;
+
+        pc.itemAvailable = this.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerCarry pc = other.GetComponent<PlayerCarry>();
+
+        if (pc == null)
+            return;
+
+        pc.itemAvailable = null;
+    }
+}
---
+++ Puzzle/Assets/Scripts/Player/PlayerCarry.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarry : MonoBehaviour
+{
+    public GameObject itemAvailable;
+    bool carrying = false;
+
+    CapsuleCollider capsuleCollider;
+
+    private void Awake()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (carrying)
+            {
+                Drop();
+            }
+            else
+            {
+                PickUp();
+            }
+        }
+    }
+
+    void PickUp ()
+    {
+        if (itemAvailable == null) return;
+
+        itemAvailable.GetComponent<SphereCollider>().enabled = false;
+
+        itemAvailable.transform.parent = transform;
+        itemAvailable.transform.localPosition = new Vector3(0, itemAvailable.GetComponent<BoxCollider>().size.y /2, capsuleCollider.radius + 0.1f + itemAvailable.GetComponent<BoxCollider>().size.z / 2);
+        itemAvailable.transform.localRotation = Quaternion.identity;
+
+        carrying = true;
+    }
+
+    void Drop ()
+    {
+        if (itemAvailable == null) return;
+
+        itemAvailable.GetComponent<SphereCollider>().enabled = true;
+
+        itemAvailable.transform.parent = GameManager.Instance.props;
+        carrying = false;
+    }
+}
