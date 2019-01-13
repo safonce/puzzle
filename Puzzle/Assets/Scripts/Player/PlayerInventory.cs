96d057673f89e7daba910ca795a51491dc430d7f
safonce <saf.once28@gmail.com>
2019-01-13 15:22:15 +0800

    Add player Inventory

---
+++ Puzzle/Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public GameObject availableDoor = null;
+
+    public Key item;
+
+    private void Update()
+    {
+       if (availableDoor & Input.GetKeyUp(KeyCode.E))
+        {
+            UseItem();
+        }
+    }
+
+    public void UseItem ()
+    {
+        if (item.triggerableObject != availableDoor)
+            return;
+
+        availableDoor.GetComponentInParent<BoxCollider>().enabled = false;
+
+        GameManager.Instance.OpenDoor(availableDoor);
+
+        item = null;
+    }
+
+    public bool IsEmpty()
+    {
+        return item == null;
+    }
+}
