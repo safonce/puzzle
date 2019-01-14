8f1a86319770e459540277d6a706c426bf807962
safonce <saf.once28@gmail.com>
2019-01-14 21:38:03 +0800

    Add dead & win zone trigger script

---
+++ Puzzle/Assets/Scripts/Level/WinZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinZone : MonoBehaviour
+{
+    int countPlayer = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        countPlayer++;
+        if (countPlayer == 2)
+            GameManager.Instance.Win();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        countPlayer--;
+        if (countPlayer < 0)
+            countPlayer = 0;
+    }
+}
