f6409dd26806811e316969b65fac50f4b86e6ec2
safonce <saf.once28@gmail.com>
2019-01-15 22:57:14 +0800

    Add pressure plate script for bridge

---
+++ Puzzle/Assets/Scripts/Level/PressurePlateBridge.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateBridge : MonoBehaviour
+{
+    [SerializeField] GameObject triggerableObject = null;
+    [SerializeField] string pressParam = "press";
+    [SerializeField] bool needWeight = true;
+
+    Animator animator;
+
+    int pressObjectCount = 0;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+            return;
+
+        pressObjectCount++;
+
+        animator.SetTrigger(pressParam);
+
+        if (triggerableObject != null)
+            GameManager.Instance.OpenBridge(triggerableObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.isTrigger)
+            return;
+
+        pressObjectCount--;
+        if (pressObjectCount > 0) return;
+
+        if (needWeight)
+        {
+            animator.SetTrigger(pressParam);
+
+            if (triggerableObject != null)
+                GameManager.Instance.CloseBridge(triggerableObject);
+        }
+    }
+}
